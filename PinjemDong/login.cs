4d3c6e6b8e143a50fd0c9095cfdf3b234c3d28a1
nasywarps <nasywarahmadhaniputrisetiawan@mail.ugm.ac.id>
2024-10-20 17:12:32 +0700

    feat: update login page

--- PinjemDong/login.cs
+++ PinjemDong/login.cs
@@ -16,5 +16,50 @@ namespace PinjemDong
         {
             InitializeComponent();
         }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void login_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Log In successful");
+            homePage home = new homePage();
+            home.Show();
+            this.Hide();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            signUp signUpPage = new signUp();
+            signUpPage.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
