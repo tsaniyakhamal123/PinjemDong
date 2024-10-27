7f53d7e1e12af13737e4d020bf28ee75f0842698
tsaniyakhamal123 <tsaniyakhamal@gmail.com>
2024-10-27 14:14:47 +0700

    committ all

---
+++ PinjemDong/testHomeAdmin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PinjemDong
+{
+    public partial class testHomeAdmin : Form
+    {
+        public testHomeAdmin()
+        {
+            InitializeComponent();
+
+        }
+
+        private void hargaBarang_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void namaBarang_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
